9cb4f1b4a9bc944ba9701799a1b2ab83c19b17fd
Jack Del Aguila <jackdelaguilavilla03@gmail.com>
2025-08-23 20:00:25 -0500

    add JWT configuration and token service for secure media vault

--- api/src/main/kotlin/com/acme/vault/SecureMediaVaultApplication.kt
+++ api/src/main/kotlin/com/acme/vault/SecureMediaVaultApplication.kt
@@ -1,9 +1,11 @@
 package com.acme.vault
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class SecureMediaVaultApplication
 
 fun main(args: Array<String>) {
---
+++ api/src/main/kotlin/com/acme/vault/config/JwtProperties.kt
@@ -0,0 +1,10 @@
+package com.acme.vault.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "jwt")
+data class JwtProperties(
+    val key: String,
+    val accessTokenExpiration: Long,
+    val refreshTokenExpiration: Long
+)
