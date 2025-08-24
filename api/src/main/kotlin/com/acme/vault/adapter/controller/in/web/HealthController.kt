4acb8fcd89c6d5e38859078af7fb9675cc2119cb
Jack Del Aguila <jackdelaguilavilla03@gmail.com>
2025-08-23 19:59:39 -0500

    add HealthController to provide health check endpoint

---
+++ api/src/main/kotlin/com/acme/vault/adapter/controller/in/web/HealthController.kt
@@ -0,0 +1,12 @@
+package com.acme.vault.adapter.controller.`in`.web
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+
+@RestController
+class HealthController {
+
+    @GetMapping("/health")
+    fun health(): Mono<Map<String, String>> = Mono.just(mapOf("status" to "UP"))
+}
